0e0d6a1537d662a8b3903fa480f4508f92bc18fc
Lily <87243032+ElleLeonne@users.noreply.github.com>
2023-03-26 12:18:06 -0500

    Add files via upload

---
+++ vit.py
@@ -0,0 +1,93 @@
+import math
+import torch
+from torch import nn
+
+"""missing blocks, missing encoder"""
+
+class SwiGLU(nn.Module):
+    def forward(self, x):
+        x, gate = x.chunk(2, dim=-1)
+        return torch.nn.functional.silu(gate) * x
+    
+class PatchEmbeddings(nn.Module):
+    """Convert image into patches, then project them into vector space"""
+    def __init__(self, config):
+        super().__init__() #Calls the __init__ of config as the Superclass, inheriting its properties.
+        self.image_size = config["image_size"] #Config appears to be a dictionary?
+        self.patch_size = config["patch_size"]
+        self.num_channels = config["num_channels"]
+        self.hidden_size = config["hidden_size"]
+        # Calculate the number of patches from the image size and patch size
+        self.num_patches = (self.image_size // self.patch_size) ** 2
+        # Create a projection layer to convert the image into patches
+        # The layer projects each patch into a vector of size hidden_size
+        self.projection = nn.Conv2d(self.num_channels, self.hidden_size, kernel_size=self.patch_size, stride=self.patch_size)
+
+    def forward(self, x):
+        # (batch_size, num_channels, image_size, image_size) -> (batch_size, num_patches, hidden_size)
+        x = self.projection(x)
+        x = x.flatten(2).transpose(1, 2)
+        return x
+    
+class Embeddings(nn.Module):
+    """Combine the patch embeddings with the class token and position embeddings."""
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.patch_embeddings = PatchEmbeddings(config)
+        # Create a learnable [CLS] token
+        # Similar to BERT, the [CLS] token is added to the beginning of the input sequence
+        # and is used to classify the entire sequence
+        self.cls_token = nn.Parameter(torch.randn(1, 1, config["hidden_size"]))
+        # Create position embeddings for the [CLS] token and the patch embeddings
+        # Add 1 to the sequence length for the [CLS] token
+        self.position_embeddings = \
+            nn.Parameter(torch.randn(1, self.patch_embeddings.num_patches + 1, config["hidden_size"]))
+        self.dropout = nn.Dropout(config["hidden_dropout_prob"])
+
+    def forward(self, x):
+        x = self.patch_embeddings(x)
+        batch_size, _, _ = x.size()
+        # Expand the [CLS] token to the batch size
+        # (1, 1, hidden_size) -> (batch_size, 1, hidden_size)
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)
+        # Concatenate the [CLS] token to the beginning of the input sequence
+        # This results in a sequence length of (num_patches + 1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.position_embeddings
+        x = self.dropout(x)
+        return x
+
+class ViTForClassfication(nn.Module):
+    """
+    The classification layer is a fully connected layer that takes the [CLS] embedding as input and 
+    outputs logits for each image. The following code implements the ViT model for image classification:
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.image_size = config["image_size"]
+        self.hidden_size = config["hidden_size"]
+        self.num_classes = config["num_classes"]
+        # Create the embedding module
+        self.embedding = Embeddings(config)
+        # Create the transformer encoder module
+        self.encoder = Encoder(config)
+        # Create a linear layer to project the encoder's output to the number of classes
+        self.classifier = nn.Linear(self.hidden_size, self.num_classes)
+        # Initialize the weights
+        self.apply(self._init_weights)
+
+    def forward(self, x, output_attentions=False):
+        # Calculate the embedding output
+        embedding_output = self.embedding(x)
+        # Calculate the encoder's output
+        encoder_output, all_attentions = self.encoder(embedding_output, output_attentions=output_attentions)
+        # Calculate the logits, take the [CLS] token's output as features for classification
+        logits = self.classifier(encoder_output[:, 0])
+        # Return the logits and the attention probabilities (optional)
+        if not output_attentions:
+            return (logits, None)
+        else:
+            return (logits, all_attentions)
